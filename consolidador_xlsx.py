570a485f609794947c6d3af434562b0f47f43b26
Wellinton Ferreira <160875820+wellintonferreira@users.noreply.github.com>
2024-03-19 20:23:01 -0300

    Add files via upload

---
+++ consolidador_xlsx.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_exel(r'D:\Wellinton\Python\PrefeituraSP\XLS\2005_sample.xlsx')
+df
+
+#sheet1 = list(df.keys())[0]
+#xls = pd.ExcelFile('2006_guias_de_itbi_pagas_2006.xlsx')
+#xls = pd.read_excel("2006_guias_de_itbi_pagas_2006.xlsx")
+#xls
+#planilha_df = pd.read_excel(xls, 'JAN-2006')
+#planilha_df
